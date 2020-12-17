1b3df56f574b23399971bc563cbfce4cc8c4aacf
bakenNNN <76163342+bakenNNN@users.noreply.github.com>
2020-12-17 14:18:12 +0100

    Add files via upload

---
+++ tror/ReleaseVersions.lua
@@ -0,0 +1,17 @@
+  
+{
+    controlpanel={
+        ver = "0.1.0",
+        file = "mainscreen",
+        manifest = {"/tror/mainscreen",
+                    "/tror/mainscreengraphics.lua"
+    },
+    launcher={
+        ver = "1.0.0",
+        file = "tro",
+        manifest = {"/tror/tro",
+                    "/tror/startupscreen.ff",
+                    "/tror/autorun.lua",
+		     "/lib/buttonAPI.lua"}
+    },
+}
